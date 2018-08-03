f46049142cdabf9a910bb72085522505fd9c4299
Alex Hunter <alexander.hunter.80@gmail.com>
2018-08-03 11:14:37 -0700

    with angular/material

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,23 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SplashComponent } from './splash/splash.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SplashComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    MaterialImportsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ public/src/app/material-imports/material-imports.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { 
+  MatButtonModule, 
+  MatCheckboxModule
+ } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  declarations: []
+})
+export class MaterialImportsModule { }
